2a9ed08d672c81a0c41d24b09b93b9b65b6deb4a
Max Luzuriaga <max@luzuriaga.com>
2013-11-18 17:39:55 -0500

    Database library for running queries against Postgres one at a time, or groups of them either in order or asynchronously.

--- app/controller.js
+++ app/controller.js
@@ -1,28 +1,14 @@
 var helper = require("../lib/helper"),
-    pg = require('pg');
+    db = require("../lib/db");
 
 function index(response, postData) {
-  pg.connect(process.env.DATABASE_URL, function(error, client, done) {
-    if(error) {
-      console.log(error);
-    } else {
-      client.query("INSERT INTO ideas (name) VALUES ('test')", function(err, result) {
-        if(err) {
-          console.log(err);
-        } else {
-          client.query('SELECT COUNT(id) as count FROM ideas', function(err, result) {
-            if(err) {
-              console.log(err);
-            } else {
-              done();
-              var message = 'There are ' + result.rows[0].count + ' ideas in the database.';
-              helper.render("index", { msg: message }, response, 200);
-            }
-          });
-        }
-      });
-    }
-  });
+  db.perform_queries_async([
+    "INSERT INTO ideas (name) VALUES ('test')",
+    ['SELECT COUNT(id) as count FROM ideas', function(result) {
+      var message = 'There are ' + result.rows[0].count + ' ideas in the database.';
+      helper.render("index", { msg: message }, response, 200);
+    }]
+  ]);
 }
 
 function submit(response, postData) {

4bba468e883f50687ee29da98748cde257419320
Max Luzuriaga <max@luzuriaga.com>
2013-11-23 14:46:25 -0500

    Altered routing code to only accept certain request types.

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
 var controller = require("../app/controller");
 
 module.exports = {
-  "/submit": controller.submit,
-  "index": controller.index
+  "/": {
+  	action: controller.index,
+  	accept: ["GET"]
+  },
+  "/submit": {
+  	action: controller.submit,
+  	accept: ["POST"]
+  }
 }

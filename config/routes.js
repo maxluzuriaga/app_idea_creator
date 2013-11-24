07ff02286169e4d08c1a52259ab0d97da8791c03
Max Luzuriaga <max@luzuriaga.com>
2013-11-24 15:28:11 -0500

    404 error page.

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,18 @@
-var controller = require("../app/controller");
+var controller = require("../app/controller"),
+    helper = require("../lib/helper");
 
 module.exports = {
   "/": {
-  	action: controller.index,
-  	accept: ["GET"]
+    action: controller.index,
+    accept: ["GET"]
   },
   "/submit": {
-  	action: controller.submit,
-  	accept: ["POST"]
+    action: controller.submit,
+    accept: ["POST"]
+  },
+  404: {
+    action: function index(response) {
+      helper.render("404.html", null, response, 404);
+    }
   }
 }
